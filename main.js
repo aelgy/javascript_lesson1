71d5da70757c7c42058a0e5ae81abd406e5a52e5
aelgy <aelgy333@gmail.com>
2024-03-25 16:09:32 +0800

    add select section

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ function order() {
   if (str.trim() === "") return alert("請輸入姓名");
   alert(str + "，謝謝您對我們的衣服有興趣！請致電 0987-654-321，會有專人提供您報價！");
   document.getElementById('nameCheck').textContent = str;
+  const types = document.getElementById('types');
+  const typesIndex = types.selectedIndex;
+  const typesText = types.options[typesIndex].text;
+  document.getElementById('userTypes').textContent = typesText;
+  const items = document.getElementById('items');
+  const itemsIndex = items.selectedIndex;
+  const itemsText = items.options[itemsIndex].text;
+  document.getElementById('userItems').textContent = itemsText;
 }
 
 function recognize() {
