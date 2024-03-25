12366406b1b4a1eaf21b9cc975118073bdc27035
aelgy <aelgy333@gmail.com>
2024-03-26 04:58:21 +0800

    Finish select section

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 function order() {
   const str = document.getElementById('name').value;
-  if (str.trim() === "") return alert("請輸入姓名");
   alert(str + "，謝謝您對我們的衣服有興趣！請致電 0987-654-321，會有專人提供您報價！");
   document.getElementById('nameCheck').textContent = str;
   const types = document.getElementById('types');
+  const typesValue = document.getElementById('types').value;
   const typesIndex = types.selectedIndex;
   const typesText = types.options[typesIndex].text;
   document.getElementById('userTypes').textContent = typesText;
   const items = document.getElementById('items');
+  const itemsValue = document.getElementById('items').value;
   const itemsIndex = items.selectedIndex;
   const itemsText = items.options[itemsIndex].text;
   document.getElementById('userItems').textContent = itemsText;
+  if (str.trim() === "") return alert("請輸入姓名");
+  if (typesValue === "") return alert("請選擇款式");
+  if (itemsValue === "") return alert("請選擇款式");
 }
 
 function recognize() {
